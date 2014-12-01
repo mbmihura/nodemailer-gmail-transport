6695de6fe36745a4bae26a1a615da31a4aa14225
Martin Mihura <mbmihura@monits.com>
2014-11-30 22:07:46 -0300

    Emails are now send to gmail api.

--- src/gmailApiTransport.js
+++ src/gmailApiTransport.js
@@ -1,9 +1,11 @@
 'use strict';
 
 var packageData = require('../package.json');
+var google = require('googleapis');
+var gmail = google.gmail('v1');
 
-module.exports = function(options) {
-    return new GmailAPITransport(options);
+module.exports = function(config) {
+    return new GmailAPITransport(config);
 };
 
 /**
@@ -12,8 +14,15 @@ module.exports = function(options) {
  * @constructor
  * @param {Object} optional config parameter for the gmail service
  */
-function GmailAPITransport(options) {
-    this.options = options || {};
+function GmailAPITransport(config) {
+    // Validations:
+    if (!config)
+        throw new Error('No "config" param was provided.');
+    if (!config.oauth2Client)
+        throw new Error('No "oauth2Client" was provided in config param.');
+
+    // Load configuration:redirectUrl
+    this.config = config;
     this.name = packageData.version.name;
     this.version = packageData.version;
 }
@@ -62,8 +71,16 @@ GmailAPITransport.prototype.generateMessageString = function(mailStream, callbac
  * @param {Function} callback Callback function to run once the message has been sent
  */
 GmailAPITransport.prototype.sendMessage = function(mail, rawMessageString, callback) {
-    // TODO: send mail to api intead of outputting to console.
-    var data = new Buffer(rawMessageString, 'utf-8').toString();
-    console.log(data);
-    callback();
+    // Encode email using gmail's base64
+    var base64EncodedEmail = new Buffer(rawMessageString).toString('base64').replace(/\+/g, '-').replace(/\//g, '_');
+
+    // Send email using gmail's API
+    var sendParams = {
+        auth: this.config.oauth2Client,
+        userId: "me",
+        resource: {
+          raw: base64EncodedEmail
+        }
+    };
+    gmail.users.messages.send(sendParams,callback);
 };
